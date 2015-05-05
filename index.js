b7599f2e5cf531cc5f03a6e15d65041041b0bedf
Ingwie Phoenix <ingwie2000@googlemail.com>
2015-05-05 19:50:54 +0200

    Compiler state and output cache are now assigned to _compilation.

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var ojc = require("ojc").compile;
 var lu = require("loader-utils");
 var path = require("path");
 
-var ojcCache = {};
-var ojcState = {};
-
 // Compiler
 module.exports = function OJ(source,map) {
+    // Compilation-specific caching
+    if(typeof this._compilation.ojc == "undefined") {
+        this._compilation.ojc = {
+            cache: {},
+            state: {}
+        };
+    }
+
+    var ojcData = this._compilation.ojc;
+
     // Header
     var header = [
         // Import the runtime
@@ -25,9 +32,9 @@ module.exports = function OJ(source,map) {
         ].join(" "));
     }
 
-    if(typeof ojcCache[this.resourcePath] != "undefined") {
+    if(typeof ojcData.cache[this.resourcePath] != "undefined") {
         // In order to avoid re-complication, return the cached object.
-        var out = ojcCache[this.resourcePath];
+        var out = ojcData.cache[this.resourcePath];
         cb(null, out.code, out.map);
     } else {
         // The query should be - almost! - the OJ options
@@ -35,7 +42,7 @@ module.exports = function OJ(source,map) {
 
         // Compiler state. Use previous one if possible
         // `this` keeps changing, so I need another way...
-        options.state = ojcState;
+        options.state = ojcData.state;
 
         // Source map and file
         var rawFile = this.resourcePath.replace(this.options.context, "");
@@ -72,13 +79,16 @@ module.exports = function OJ(source,map) {
                     var warning = result.warnings[i];
                     _this.emitWarning(warning);
                 }
-                ojcState = result.state;
-                ojcCache[_this.resourcePath] = {
+                ojcData.state = result.state;
+                ojcData.cache[_this.resourcePath] = {
                     code: header+result.code,
                     map: result.map
                 }
                 cb(err, header+result.code, result.map);
             }
         });
+
+        // Backwards assignment
+        this._compilation.ojc = ojcData;
     }
 };
